c4d9c072e424dcc8b0cdffdf3a9e105395201015
Kartik Patil <kartikrbj10@gmail.com>
2024-09-21 18:25:20 +0530

    second day

--- src/main/java/com/example/RestApi/controller/student_activity.java
+++ src/main/java/com/example/RestApi/controller/student_activity.java
@@ -22,7 +22,24 @@ public class student_activity {
     public String insertStudent(@RequestBody studentClass student){
         student_map.put(student.getId(),student);
         return "New student is inserted";
+    }
 
+    @GetMapping("id/{myId}")
+    public studentClass getStudentById(@PathVariable Integer myId){
+        return student_map.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public String deleteStudent(@PathVariable Integer myId){
+        student_map.remove(myId);
+        return "Student with Id " + myId+ " is deleted";
 
     }
+    @PutMapping("id/{myId}")
+    public String updateStudent(@PathVariable Integer myId,@RequestBody studentClass student){
+        student_map.put(myId,student);
+        return "Student data updated successfully";
+    }
+
+
 }
