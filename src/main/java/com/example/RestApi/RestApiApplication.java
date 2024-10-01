e0cf4686deced9b03163ef536d01930390882428
Kartik Patil <kartikrbj10@gmail.com>
2024-10-01 11:45:46 +0530

    Connected to MongoDB Atlas

--- src/main/java/com/example/RestApi/RestApiApplication.java
+++ src/main/java/com/example/RestApi/RestApiApplication.java
@@ -2,15 +2,29 @@ package com.example.RestApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Bean
+	public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+		return new MongoTransactionManager(dbFactory);
+	}
+
 }
+//PlatFormTransactionManager
+//MongoTransactionManager
+
 
 
 
